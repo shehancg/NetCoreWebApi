fa57fdd1f3b048c1287a13af3adb047a455f7277
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-08-03 10:14:24 +0530

    added naviagtion property to clssroom for student

--- CoreWebApi/Migrations/20230803040556_newMigration.Designer.cs
+++ CoreWebApi/Migrations/20230803040556_newMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolManagementSystem.API.Data;
 
 namespace CoreWebApi.Migrations
 {
     [DbContext(typeof(SchoolManagementContext))]
-    partial class SchoolManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20230803040556_newMigration")]
+    partial class newMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CoreWebApi/Models/ClassroomModel.cs
+++ CoreWebApi/Models/ClassroomModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CoreWebApi.Models
@@ -11,5 +12,8 @@ namespace CoreWebApi.Models
 
         [Column(TypeName = "nvarchar(100)")]
         public string ClassroomName { get; set;}
+
+        // Navigation property for the one-to-many relationship
+        public ICollection<StudentModel> Students { get; set; }
     }
 }
