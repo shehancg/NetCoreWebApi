913d5d88c7d709e55acad75fe1a1c8d6d648f910
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-08-28 00:37:01 +0530

    Added validation for class room

--- CoreWebApi/Repository/Impl/ClassroomRepository.cs
+++ CoreWebApi/Repository/Impl/ClassroomRepository.cs
@@ -1,8 +1,12 @@
 ﻿using CoreWebApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using SchoolManagementSystem.API.Data;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace CoreWebApi.Repository.Impl
 {
@@ -25,12 +29,37 @@ namespace CoreWebApi.Repository.Impl
             return await _context.Classrooms.FindAsync(classroomId);
         }
 
-        public async Task<ClassroomModel> AddClassroomAsync(ClassroomModel classroom)
+        /*public async Task<ClassroomModel> AddClassroomAsync(ClassroomModel classroom)
         {
             _context.Classrooms.Add(classroom);
             await _context.SaveChangesAsync();
             return classroom;
+        }*/
+
+        /*This checks for classes with same name before inserting*/
+        public async Task<ClassroomModel> AddClassroomAsync(ClassroomModel classroom)
+        {
+            // Remove leading and trailing spaces from the classroom name
+            string trimmedClassroomName = classroom.ClassroomName.Trim();
+
+            // Retrieve all classrooms from the database
+            var classrooms = await _context.Classrooms.ToListAsync();
+
+            // Check if a classroom with the same name already exists (case-insensitive)
+            bool classroomExists = classrooms.Any(c => c.ClassroomName.Trim().Equals(trimmedClassroomName, StringComparison.OrdinalIgnoreCase));
+
+            if (classroomExists)
+            {
+                // Classroom with the same name already exists, return an appropriate response or throw an exception
+                throw new InvalidOperationException("A classroom with the same name already exists.");
+            }
+
+            _context.Classrooms.Add(classroom);
+            await _context.SaveChangesAsync();
+            return classroom;
         }
+
+
 
         public async Task<ClassroomModel> UpdateClassroomAsync(ClassroomModel classroom)
         {
@@ -39,7 +68,7 @@ namespace CoreWebApi.Repository.Impl
             return classroom;
         }
 
-        public async Task<bool> DeleteClassroomAsync(int classroomId)
+        /*public async Task<bool> DeleteClassroomAsync(int classroomId)
         {
             var classroom = await _context.Classrooms.FindAsync(classroomId);
             if (classroom == null)
@@ -48,6 +77,27 @@ namespace CoreWebApi.Repository.Impl
             _context.Classrooms.Remove(classroom);
             await _context.SaveChangesAsync();
             return true;
+        }*/
+
+        public async Task<bool> DeleteClassroomAsync(int classroomId)
+        {
+            var classroom = await _context.Classrooms
+                .Include(c => c.Students) 
+                .FirstOrDefaultAsync(c => c.ClassroomId == classroomId);
+
+            if (classroom == null)
+                return false;
+
+            if (classroom.Students.Any())
+            {
+                // There are enrolled students in the class, so prevent deletion
+                return false;
+            }
+
+            _context.Classrooms.Remove(classroom);
+            await _context.SaveChangesAsync();
+            return true;
         }
+
     }
 }
